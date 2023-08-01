a004ac3f03dcb3514bad37cbd42c198cff252c59
Mat Moore <matmoore@users.noreply.github.com>
2023-08-01 18:38:07 +0100

    Refactor specs

--- spec/obsidian/parser/parsed_markdown_document_spec.rb
+++ spec/obsidian/parser/parsed_markdown_document_spec.rb
@@ -1,45 +1,33 @@
 # frozen_string_literal: true
 
 RSpec.describe Obsidian::ParsedMarkdownDocument do
-  let(:index) { Obsidian::Page.create_root }
+  let(:index) { nil }
 
-  before do
-    index.add_page("foo/bar")
-    index.add_page("animals/cat")
+  def create_instance(markdown)
+    Obsidian::ObsidianFlavoredMarkdown.parse(markdown, root: index)
   end
 
   it "extracts [foo](bar) links" do
-    markdown = "here is a link: [foo](bar)"
-
-    converter = Obsidian::ObsidianFlavoredMarkdown.parse(markdown)
-    links = converter.extract_links
-
+    parsed_document = create_instance("here is a link: [foo](bar)")
+    links = parsed_document.extract_links
     expect(links).to include(["bar", "foo"])
   end
 
   it "extracts [foo](bar) links" do
-    markdown = "here is a link: [foo](bar)"
-
-    converter = Obsidian::ObsidianFlavoredMarkdown.parse(markdown)
-    links = converter.extract_links
-
+    parsed_document = create_instance("here is a link: [foo](bar)")
+    links = parsed_document.extract_links
     expect(links).to include(["bar", "foo"])
   end
 
   it "extracts links with titles" do
-    markdown = '[link](/uri "title")'
-    converter = Obsidian::ObsidianFlavoredMarkdown.parse(markdown)
-    links = converter.extract_links
-
+    parsed_document = create_instance('[link](/uri "title")')
+    links = parsed_document.extract_links
     expect(links).to include(["/uri", "link"])
   end
 
   it "extracts links with no destinations" do
-    markdown = "[link1]() [link2](<>)"
-
-    converter = Obsidian::ObsidianFlavoredMarkdown.parse(markdown)
-    links = converter.extract_links
-
+    parsed_document = create_instance("[link1]() [link2](<>)")
+    links = parsed_document.extract_links
     expect(links).to include(["", "link1"], ["", "link2"])
   end
 
@@ -50,18 +38,25 @@ RSpec.describe Obsidian::ParsedMarkdownDocument do
       [bar]: /url "title"
     END
 
-    converter = Obsidian::ObsidianFlavoredMarkdown.parse(markdown)
-    links = converter.extract_links
+    parsed_document = Obsidian::ObsidianFlavoredMarkdown.parse(markdown)
+
+    links = parsed_document.extract_links
 
     expect(links).to include(["/url", "foo"])
   end
 
-  it "extracts wikilinks" do
-    markdown = "[[animals/cat]]"
+  context "with a document tree" do
+    let(:index) { Obsidian::Page.create_root }
 
-    converter = Obsidian::ObsidianFlavoredMarkdown.parse(markdown, root: index)
-    links = converter.extract_links
+    before do
+      index.add_page("foo/bar")
+      index.add_page("animals/cat")
+    end
 
-    expect(links).to include(["animals/cat", "cat"])
+    it "extracts wikilinks" do
+      parsed_document = create_instance("[[animals/cat]]")
+      links = parsed_document.extract_links
+      expect(links).to include(["animals/cat", "cat"])
+    end
   end
 end
